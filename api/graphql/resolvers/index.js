48ca80dee4f6d28a8063d15cb7f6d7f6b9dfd374
Ben Hanna <benpaulhanna@gmail.com>
2018-02-09 21:07:38 +0000

    Updated the setup script populate chapters and verses.

--- api/graphql/resolvers/index.js
+++ api/graphql/resolvers/index.js
@@ -1,6 +1,10 @@
 const bibleService = require('../../../lib/services/bible');
+const chapterLoader = require('../../../lib/data/loaders/chapter');
 
 module.exports = {
+    Book: {
+        chapters: ({ bookId }) => chapterLoader.load(bookId)
+    },
     Query: {
         book: (_, { id }) => bibleService.getBook(id),
         books: () => bibleService.getBooks()
--- setup.js
+++ setup.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const { sourceUrl } = require('./config');
-const { connection, Book } = require('./lib/data/sequelize');
+const { connection, Book, Chapter, Verse } = require('./lib/data/sequelize');
 
 const tasks = [
     axios.get(sourceUrl),
@@ -10,12 +10,34 @@ const tasks = [
 
 Promise
     .all(tasks)
-    .then(populate);
+    .then(populateBooks);
 
-function populate([response]) {
+function populateBooks([response]) {
     const books = JSON.parse(response.data.trim());
 
     books.forEach(book => {
-        Book.create({ name: book.book });
+        Book
+            .create({ name: book.book })
+            .then(populateChapters.bind(null, book.chapters));
     });
+}
+
+function populateChapters(chapters, book) {
+    chapters.forEach(chapter => {
+        const number = Object.keys(chapter).pop();
+        const verses = chapter[number];
+        
+        Chapter
+            .create({ number })
+            .then(populateVerses.bind(null, verses));
+    });
+}
+
+function populateVerses(verses, chapter) {
+    for (let number in verses) {
+        const body = verses[number];
+        
+        Verse
+            .create({ body, number });
+    }
 }
